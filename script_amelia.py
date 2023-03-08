593ebe3ad854ca93133f9a91b35b883863d8ad7b
rcpsilva <rcpsilva@gmail.com>
2023-03-08 18:40:58 -0300

    script to run experiments in the server

---
+++ script_amelia.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import make_friedman2
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
+from sklearn.gaussian_process.kernels import ConstantKernel, RBF
+import pandas as pd
+import numpy as np
+import pickle
+
+#nohup python script_amelia.py > output.log & 
+
+df = pd.read_csv('modeloJoinville.csv')
+
+df = df.dropna()
+df = df.sample(n = 200)
+
+X = df[['lat', 'lon']].to_numpy()
+y1 = df['lat_api'].to_numpy()
+y2 = df['lon_api'].to_numpy()
+
+regressorLat = GaussianProcessRegressor()
+regressorLon = GaussianProcessRegressor()
+
+regressorLat.fit(X, y1)
+regressorLon.fit(X, y2)
+
+with open('models.pkl', 'wb') as file:
+    pickle.dump(regressorLat, file)
+    pickle.dump(regressorLon, file)
+
+file.close()
+
