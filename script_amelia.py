fac3289af32fe74dcd6e4dfc69ea6b715e809fde
rcpsilva <rcpsilva@gmail.com>
2023-03-08 19:02:17 -0300

    Update script_amelia.py

--- script_amelia.py
+++ script_amelia.py
@@ -5,13 +5,16 @@ from sklearn.gaussian_process.kernels import ConstantKernel, RBF
 import pandas as pd
 import numpy as np
 import pickle
+import sys
 
 #nohup python script_amelia.py > output.log & 
+
+n = int(sys.argv[1])
 
 df = pd.read_csv('modeloJoinville.csv')
 
 df = df.dropna()
-df = df.sample(n = 2000)
+df = df.sample(n = n)
 
 X = df[['lat', 'lon']].to_numpy()
 y1 = df['lat_api'].to_numpy()
